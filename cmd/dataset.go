3da9a28611e2872c02d63f598e7bda4a8845c3c7
marvinfpl <marvinfoppolo@gmail.com>
2026-05-05 14:28:43 +0200

    unzip command

--- cmd/dataset.go
+++ cmd/dataset.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/xyproto/unzip"
 	"os/exec"
 	"fmt"
 )
@@ -33,6 +34,24 @@ var downloadCommand = &cobra.Command{
 		_, err := exec.Command("kaggle", "datasets", "download", "-d", args[0]).CombinedOutput()
 		if err != nil {
 			return fmt.Errorf("Invalid command: %v", err)
+		}
+		return nil
+	},
+}
+
+var unzipCmd = &cobra.Command{
+	Use: "unzip",
+	Short: "Unzip a given file, in a given destination",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("Unexpected number of arguments: got %d, want 1", len(args))
+		}
+		cwd, err := exec.Command("pwd").CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Could not get current working directory: %v", err)
+		}
+		if err = unzip.Extract(args[0], (string)(cwd)); err != nil {
+			return fmt.Errorf("Failed to unzip the dataset: %v", err)
 		}
 		return nil
 	},
