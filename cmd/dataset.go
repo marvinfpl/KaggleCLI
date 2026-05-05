418e0964f7d47a02eb2ed349e04c68481ec66b66
marvinfpl <marvinfoppolo@gmail.com>
2026-05-05 14:11:16 +0200

    download command

--- cmd/dataset.go
+++ cmd/dataset.go
@@ -6,15 +6,34 @@ import (
 	"fmt"
 )
 
+
 var listCmd = &cobra.Command{
 	Use: "list",
 	Short: "Returns a list of datasets",
 	RunE: func (cmd *cobra.Command, args[] string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("Unexpected number of arguments: got %d, want 1", len(args))
+		}
 		output, err := exec.Command("kaggle", "datasets", "list", args[0]).CombinedOutput()
 		if err != nil {
 			return fmt.Errorf("Invalid command: %v", err)
 		}
 		fmt.Println((string)(output))
+		return nil
+	},
+}
+
+var downloadCommand = &cobra.Command{
+	Use: "download",
+	Short: "Download the dataset corresponding to the given id",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return fmt.Errorf("Unexpected number of arguments: got %d, want 1", len(args))
+		}
+		_, err := exec.Command("kaggle", "datasets", "download", "-d", args[0]).CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("Invalid command: %v", err)
+		}
 		return nil
 	},
 }
